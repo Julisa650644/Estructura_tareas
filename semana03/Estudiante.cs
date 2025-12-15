676756a2d1eddcb60612fdc2ad585e4729ba8b0d
Josselin123-coder <gabymerin1996@gmail.com>
2025-12-14 22:42:52 -0500

    hecho
    hecho

---
+++ semana03/Estudiante.cs
@@ -0,0 +1,8 @@
+public class Estudiante
+{
+    public int ID;
+    public string Nombres;
+    public string Apellidos;
+    public string Direccion;
+    public string[] Telefonos = new string[3];
+}
---
+++ semana03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Estudiante est = new Estudiante();
+
+        Console.Write("ID: ");
+        est.ID = int.Parse(Console.ReadLine());
+
+        Console.Write("Nombres: ");
+        est.Nombres = Console.ReadLine();
+
+        Console.Write("Apellidos: ");
+        est.Apellidos = Console.ReadLine();
+
+        Console.Write("Dirección: ");
+        est.Direccion = Console.ReadLine();
+
+        Console.WriteLine("Ingrese los 3 teléfonos:");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Teléfono {i + 1}: ");
+            est.Telefonos[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("\n--- Datos Registrados ---");
+        Console.WriteLine($"ID: {est.ID}");
+        Console.WriteLine($"Nombres: {est.Nombres}");
+        Console.WriteLine($"Apellidos: {est.Apellidos}");
+        Console.WriteLine($"Dirección: {est.Direccion}");
+
+        Console.WriteLine("Teléfonos:");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"  {est.Telefonos[i]}");
+        }
+    }
+}
